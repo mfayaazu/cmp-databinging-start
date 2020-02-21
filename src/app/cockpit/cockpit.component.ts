296dae99c023d75072cc96f989a853a26f67d32f
Fayaazuddin Ali Mohammad <fayaazuddin.ali.mohammad@wirelesscar.com>
2020-02-21 16:39:18 +0100

    first commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,35 @@
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(this.serverContentInput);
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
+}
---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,84 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component, ContentChild,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
+
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
+  @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
+
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnchanges called');
+    console.log(changes);
+
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+    console.log('textContent ' + this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph: '+ this.paragraph.nativeElement.textContent)
+
+
+  }
+
+  ngDoCheck(): void {
+    console.log('ngdocheck Called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterinit Called');
+    console.log('Text Content of paragraph: '+ this.paragraph.nativeElement.textContent)
+
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterInitChecked called');
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewinit Called');
+    console.log('textContent ' + this.header.nativeElement.textContent);
+
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+  }
+
+
+}
